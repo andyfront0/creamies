5c15304f26d352f8272a8e83f9784d578ae43cde
andyfront0 <87222483+andyfront0@users.noreply.github.com>
2021-11-27 10:27:09 +0100

    MetaMask

---
+++ script.js
@@ -0,0 +1,70 @@
+//On Load
+window.addEventListener('DOMContentLoaded', () => {
+
+    window.userWalletAddress = null;
+    const connectButton = document.getElementById('connectButton');
+    const connectedText = document.getElementById('connectedText');
+    const addressText = document.getElementById('addressText');
+
+
+    //Check if MetaMask Wallet is installed
+    function checkMetaMask() {
+        if (!window.ethereum) {
+            connectButton.innerText = "Install MetaMask!";
+            connectButton.style.fontSize = "min(3vw, 15px)";
+        } else {
+            connectButton.addEventListener('click', connectMetaMask);
+        }
+    }
+
+    //Connect to MetaMask
+    async function connectMetaMask() {
+        //Fetch accounts
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+            .catch((e) => {
+                console.log("User rejected");
+                return;
+            })
+
+        //If no accounts (this needs a seperate return, bc of async call)
+        if (!accounts) { return; }
+
+        //If accounts found
+        window.userWalletAddress = accounts[0];
+        connectedText.innerText = "Connected!";
+        addressText.innerText = window.userWalletAddress;
+        connectButton.innerText = 'Disconnect';
+
+        connectButton.removeEventListener('click', connectMetaMask)
+        setTimeout(() => {
+            connectButton.addEventListener('click', disconnectMetaMask)
+        }, 200)
+    }
+
+    function disconnectMetaMask() {
+        window.userWalletAddress = null
+        connectedText.innerText = "";
+        addressText.innerText = '';
+        connectButton.innerText = 'Connect';
+
+        connectButton.removeEventListener('click', disconnectMetaMask)
+        setTimeout(() => {
+            connectButton.addEventListener('click', connectMetaMask)
+        }, 200)
+    }
+
+    //Do this first
+    checkMetaMask();
+
+    //If MetaMask account changes
+    window.ethereum.on("accountsChanged", accounts => {
+        if (accounts.length > 0)
+            console.log(`Account connected: ${accounts[0]}`);
+        else {
+            console.log("Account disconnected");
+            disconnectMetaMask();
+        }
+    });
+
+
+});
